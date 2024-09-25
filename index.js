dfb2eea1d16f7a739925b6a61d9fbd9a5c544f38
Mohammad Zakaria <mdzakaria3010@gmail.com.com>
2024-09-25 22:29:21 +0600

    history itms complet

--- index.js
+++ index.js
@@ -34,30 +34,55 @@ calcolatebutton.addEventListener("click", function () {
     mybalanceElement.innerText = newBalance;
     const modal = document.getElementById("my_modal_4");
     modal.showModal();
+    const historyItems = document.createElement("div");
+    historyItems.classList.add(
+        "items-center",
+        "mx-auto",
+        "font-Manrope",
+        "w-9/12",
+        "p-8",
+        "rounded-md",
+        "border-2",
+        "border-black",
+        "mt-8"
+    );
+
+    
+    historyItems.innerHTML = `
+     <div class="flex font-bold text-2xl"> <h1>${donatebalance.toFixed(2)}</h1>
+      <h1>${donatearia}</h1></div>
+      <p class=" m-2 font-boldl">${new Date().toDateString()}</p>
+    `;
+
+    
+    donationHistory.insertBefore(historyItems, donationHistory.firstChild);
   
 });
 
 
 const history = document.getElementById("History");
-const Donation = document.getElementById("Donation");
-const DonationHistoyr = document.getElementById("DonationHistoyr");
-const Donatearia = document.getElementById("Donatearia");
+const donation = document.getElementById("Donation");
+const donationHistory = document.getElementById("DonationHistoyr");
+const donatearia = document.getElementById("Donatearia").innerText;
+const donatebalance = parseFloat(document.getElementById("donatebalance").value);
+const mybalanceElement = document.getElementById("mybalance"); 
+const mybalance = parseFloat(mybalanceElement.innerText); 
+
 
 history.addEventListener("click", function () {
     const heddin = document.getElementById("heddin");
+
+    
     heddin.classList.add("hidden");
 
-    
-
+   
     history.classList.add("p-4", "rounded-md", "bg-lime-400");
-    Donation.classList.remove(  "bg-lime-400");
-    DonationHistoyr.classList.remove(  "hidden");
+
+   
+    donation.classList.remove("bg-lime-400");
 
 
-    DonationHistoyr.createElement("div");
-    new Date()
-
-    
+    donationHistory.classList.remove("hidden");
 });
 
-console.log(heddin);
+
